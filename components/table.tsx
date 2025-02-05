81c99ac78728a0766f432eb1e1905bbd33aa0aa7
werapol <wesakaber@gmail.com>
2025-02-05 09:20:46 +0700

    fix:setup and install tool in project

---
+++ components/table.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function table() {
+  return (
+    <div className='text-xl text-red-500'>table</div>
+  )
+}
+
+export default table
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,10 @@
-import Image from "next/image";
-import localFont from "next/font/local";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import DataTable from "@/components/table";
 
 export default function Home() {
-  return <div>เริ่มโปรเจค</div>;
+  return (
+    <div>
+      เริ่มโปรเจค
+      <DataTable/>
+    </div>
+  );
 }
